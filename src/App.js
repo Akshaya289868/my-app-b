a80c386b147eabcf4c8a49cb31c374123742452d
Akshaya_289868 <akshayagorrepati868@gmail.com>
2025-03-21 12:21:03 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import Footer from "./components/Footer";
+import Cart from "./components/Cart"
+import Login from "./components/Login"
+import Register from "./components/Register";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+          <Route index element={<Products />} />
+          <Route path="home" element={<Products />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+      </Routes> 
+      <Footer/>
+    </BrowserRouter>
   );
 }
-
 export default App;
